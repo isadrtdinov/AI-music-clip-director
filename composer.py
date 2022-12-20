7a6de86492909872873c45d1eb5431decf122935
LAPTOP-D26A8D2K\Ilya Glazkov <glazkovii2005@gmail.com>
2022-12-20 21:12:25 +0500

    add properties to config.yaml

--- composer.py
+++ composer.py
@@ -3,36 +3,94 @@ from kandinsky2 import get_kandinsky2
 from align_segments import align_segments
 from get_song import get_lyrics
 from separate_vocals import separate_vocals
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+import cv2
+import os
+import numpy as np
 
 
 class ClipDirector(object):
     def __init__(self, device,
                  whisper_size: str = 'medium', whisper_beam_size: int = 10,
-                 fps: int = 20):
+                 fps: int = 20, image_height: int = 512, image_width: int = 512, with_img2img_transition: bool = True,
+                 kandinsky_images_steps: int = 75, kandinsky_flow_steps: int = 100,
+                 kandinsky_denoised_type: str = "dynamic_threshold", kandinsky_dynamic_threshold_v: float = 99.5,
+                 kandinsky_sampler: str = "kandinsky_sampler", kandinsky_ddim_eta: float = 0.05,
+                 kandinsky_guidance_scale: float = 7, kandinsky_strength: float = 0.9, kandinsky_progress: bool = True,
+                 prompt_perturbation: float = 0.25):
         self.whisper_size = whisper_size
         self.whisper_beam_size = whisper_beam_size
+        self.fps = fps
+        self.image_height = image_height
+        self.image_width = image_width
+        self.with_img2img_transition = with_img2img_transition
+        self.kandinsky_images_steps = kandinsky_images_steps
+        self.kandinsky_flow_steps = kandinsky_flow_steps
+        self.kandinsky_denoised_type = kandinsky_denoised_type
+        self.kandinsky_dynamic_threshold_v = kandinsky_dynamic_threshold_v
+        self.kandinsky_sampler = kandinsky_sampler
+        self.kandinsky_ddim_eta = kandinsky_ddim_eta
+        self.kandinsky_guidance_scale = kandinsky_guidance_scale
+        self.kandinsky_strength = kandinsky_strength
+        self.kandinsky_progress = kandinsky_progress
+        self.prompt_perturbation = prompt_perturbation
 
         self.kandinsky = get_kandinsky2(device, task_type='text2img')
         self.whisper = whisper.load_model(whisper_size)
 
-    def generate_images(self, prompts: list):
+    def generate_images(self, prompts: list, times: list):
         pil_imgs = []
         for prompt in prompts:
-            pil_img = self.kandinsky.generate_text2img(prompt, batch_size=1, h=512, w=512, num_steps=75,
-                                                       denoised_type='dynamic_threshold', dynamic_threshold_v=99.5,
-                                                       sampler='ddim_sampler', ddim_eta=0.05, guidance_scale=7)
+            pil_img = self.kandinsky.generate_text2img(prompt, batch_size=1, h=self.image_height, w=self.image_width,
+                                                       num_steps=self.kandinsky_images_steps,
+                                                       denoised_type=self.kandinsky_denoised_type,
+                                                       dynamic_threshold_v=self.kandinsky_dynamic_threshold_v,
+                                                       sampler=self.kandinsky_sampler, ddim_eta=self.kandinsky_ddim_eta,
+                                                       guidance_scale=self.kandinsky_guidance_scale)
             pil_imgs.append(pil_img[0])
         scenario = []
-        for i in range(len(prompts)):
-            scenario += [{
-                'frame': i * 30, 'image': pil_imgs[i], 'prompt': prompts[i],
-                'perturbation_peaks': [i * 30 + 15], 'prompt_perturbation': 0.25,
-            }]
+        texts = []
+        if self.with_img2img_transition:
+            last = 0
+            s = 0
+            for i in range(len(prompts)):
+                s += len(prompts[i])
+            s /= len(prompts)
+            s /= 2
+
+            for i in range(len(prompts)):
+                peaks = []
+
+                scenario += [{
+                    'frame': int(last + self.fps * times[i] / 2), 'image': pil_imgs[i], 'prompt': prompts[i],
+                    'perturbation_peaks': [int(last + self.fps * times[i] / 2)],
+                    'prompt_perturbation': self.prompt_perturbation,
+                }]
+                for j in range(self.fps * times[i]):
+                    texts.append(prompts[i])
+                last += int(self.fps * times[i])
+        else:
+            for i in range(len(prompts)):
+                last = 0
+                while last <= self.fps * times[i]:
+                    scenario.append((pil_imgs[i], prompts[i]))
+                    last += 1
+            return scenario
         all_images = self.kandinsky.generate_img2img_flow(scenario,
-                                                          strength=0.9, num_steps=100, guidance_scale=7, progress=True,
-                                                          dynamic_threshold_v=99.5, denoised_type='dynamic_threshold',
-                                                          sampler='ddim_sampler', ddim_eta=0.05)
-        return all_images
+                                                          strength=self.kandinsky_strength,
+                                                          num_steps=self.kandinsky_flow_steps,
+                                                          guidance_scale=self.kandinsky_guidance_scale,
+                                                          progress=self.kandinsky_progress,
+                                                          dynamic_threshold_v=self.kandinsky_dynamic_threshold_v,
+                                                          denoised_type=self.kandinsky_denoised_type,
+                                                          sampler=self.kandinsky_sampler,
+                                                          ddim_eta=self.kandinsky_ddim_eta)
+        ans = []
+        for i in range(len(texts)):
+            ans.append((all_images[i], texts[i]))
+
+        return ans
 
     def generate_alignment(self, song_file: str, lyrics_str: str, language: str):
         options = dict(language=language, beam_size=self.whisper_beam_size,
@@ -45,8 +103,35 @@ class ClipDirector(object):
         lyrics, language = get_lyrics(query, song_file, ya_music_token, genius_token)
         return lyrics, language
 
-    def separae_vocals(self, song_file: str, out_file: str, out_sample_rate: int):
+    def separate_vocals(self, song_file: str, out_file: str, out_sample_rate: int):
         separate_vocals(song_file=song_file, out_file=out_file, out_sample_rate=out_sample_rate)
 
-    def create_video_clip(self):
-        pass
+    def add_caption_2_image(self, img, caption):
+        MAX_W, MAX_H = self.image_width, self.image_height
+        l = int(0.04 * MAX_W)
+        p = textwrap.wrap(caption, width=int(self.image_width // (l * 0.65)))
+
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype('arial.ttf', l)
+
+        current_h, pad = int(0.7 * MAX_H), int(0.02 * MAX_H)
+        for line in p:
+            w, h = draw.textsize(line, font=font)
+            draw.text(((MAX_W - w) // 2, current_h), line, fill='white', font=font, stroke_width=2, stroke_fill='black')
+            current_h += h + pad
+        return img
+    def create_video_clip(self, images_with_texts):
+        all_images = []
+        for i in range(len(images_with_texts)):
+            all_images.append(self.add_caption_2_image(images_with_texts[i][0], images_with_texts[i][1]))
+        cv2_images = [np.array(image)[..., ::-1].copy() for image in all_images]
+        height, width, layers = cv2_images[0].shape
+
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        video = cv2.VideoWriter('video.mp4', fourcc, self.fps, (height, width))
+
+        for image in cv2_images:
+            video.write(image)
+
+        cv2.destroyAllWindows()
+        video.release()
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="AI-music-clip-director")
-parser.add_argument("--query", dest="query", required=True)
-parser.add_argument("--style", dest="style", required=True)
-parser.add_argument("--config", dest="config", required=True)
-parser.add_argument("--ya_music_token", dest="ya_music_token", required=True)
-parser.add_argument("--genius_token", dest="genius_token", required=True)
+parser.add_argument("--query", dest="query", required=True, type=str)
+parser.add_argument("--style", dest="style", required=True, type=str)
+parser.add_argument("--config", dest="config", required=False, default="config.yaml", type=str)
+parser.add_argument("--ya_music_token", dest="ya_music_token", required=True, type=str)
+parser.add_argument("--genius_token", dest="genius_token", required=True, type=str)
 
 args = parser.parse_args()
 
 from composer import ClipDirector
-from separate_vocals import separate_vocals
 
 import io
 import os
@@ -33,26 +32,50 @@ from tqdm.notebook import tqdm
 import yaml
 
 
-def load_config():
-    stream = open("config.yaml", 'r')
+def load_config(config_file):
+    stream = open(config_file, 'r')
     dictionary = yaml.safe_load(stream)
     return dictionary
 
 
-dictionary = load_config()
+dictionary = load_config(config_file=args.config)
 out_sample_rate = int(dictionary['out_sample_rate'])
 song_file = dictionary['song_file']
 vocals_file = dictionary['vocals_file']
 whisper_size = dictionary['whisper_size']
+image_height = int(dictionary['image_height'])
+image_width = int(dictionary['image_width'])
+fps = int(dictionary['fps'])
+kandinsky_images_steps = int(dictionary['kandinsky_images_steps'])
+kandinsky_flow_steps = int(dictionary['kandinsky_flow_steps'])
+kandinsky_denoised_type = dictionary['kandinsky_denoised_type']
+kandinsky_dynamic_threshold_v = float(dictionary['kandinsky_dynamic_threshold_v'])
+kandinsky_sampler = dictionary['kandinsky_sampler']
+kandinsky_ddim_eta = float(dictionary['kandinsky_ddim_eta'])
+kandinsky_guidance_scale = float(dictionary['kandinsky_guidance_scale'])
+kandinsky_strength = float(dictionary['kandinsky_strength'])
+kandinsky_progress = bool(dictionary['kandinsky_progress'])
+kandinsky_prompt_perturbation = float(dictionary['prompt_perturbation'])
+with_img2img_transition = bool(dictionary['with_img2img_transition'])
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-clip_director = ClipDirector(device=DEVICE, whisper_size=whisper_size, whisper_beam_size=10, fps=20)
+clip_director = ClipDirector(device=DEVICE, whisper_size=whisper_size, whisper_beam_size=10, fps=fps,
+                             image_height=image_height, image_width=image_width,
+                             with_img2img_transition=with_img2img_transition,
+                             kandinsky_images_steps=kandinsky_images_steps, kandinsky_flow_steps=kandinsky_flow_steps,
+                             kandinsky_denoised_type=kandinsky_denoised_type,
+                             kandinsky_dynamic_threshold_v=kandinsky_dynamic_threshold_v,
+                             kandinsky_sampler=kandinsky_sampler, kandinsky_ddim_eta=kandinsky_ddim_eta,
+                             kandinsky_guidance_scale=kandinsky_guidance_scale, kandinsky_strength=kandinsky_strength,
+                             prompt_perturbation=kandinsky_prompt_perturbation)
 
 lyrics, language = clip_director.get_song_and_lyrics(args.query, song_file, args.ya_music_token, args.genius_token)
-clip_director.separae_vocals(song_file=song_file, out_file=vocals_file, out_sample_rate=out_sample_rate)
+clip_director.separate_vocals(song_file=song_file, out_file=vocals_file, out_sample_rate=out_sample_rate)
 
 segments = clip_director.generate_alignment(song_file=vocals_file, lyrics_str=lyrics, language=language)
 prompts = []
+times = []
 for i in segments:
     prompts.append(i[0])
-all_images = clip_director.generate_images(prompts=prompts)
+    times.append(i[1])
+all_images = clip_director.generate_images(prompts=prompts, times=times)
